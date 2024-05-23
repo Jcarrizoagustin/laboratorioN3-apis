8f91c98197387bd390d77050980cdca2db26698f
Daniel_juarez97 <juarezz.daniel97@gmail.com>
2024-05-23 17:35:52 -0300

    Funcionalidades Orden y DetallesOrden Aún no anda bien

--- core/api.py
+++ core/api.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .serializers import ProductoSerializer, OrdenSerializer, DetalleOrdenSerializer
 from .models import Producto, Orden, DetalleOrden
 
@@ -13,8 +14,39 @@ class OrdenViewSet(viewsets.ModelViewSet):
     queryset = Orden.objects.all()
     serializer_class = OrdenSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        instance  = self.get_object()
+        for detalle in instance.DetalleOrden.all():
+            detalle.delete()
+        return super().destroy(request, *args, **kwargs)
+
 
 
 class DetalleOrdenViewSet(viewsets.ModelViewSet):
     queryset = DetalleOrden.objects.all()
     serializer_class = DetalleOrdenSerializer
+
+    def create(self, request, *args, **kwargs):
+        orden = request.data.get('orden')
+        cantidad = request.get('cantidad')
+        producto = request.data.get('producto')
+
+        detalle = DetalleOrden.objects.filter(orden_id=orden, producto_id=producto).first()
+        if detalle:
+            detalle.cantidad += int(cantidad)
+            detalle.save()
+            return Response(status=status.HTTP_200_OK)
+
+        return super().create(request, *args, **kwargs)
+    
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        cantidad = request.data.get('cantidad',instance.cantidad)
+
+        if int(cantidad)<=0:
+            return Response({'error':'La cantidad debe ser mayor a 0'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        instance.cantidad = cantidad
+        instance.save()
+        
+        return super().update(request, *args, **kwargs)
--- core/serializers.py
+++ core/serializers.py
@@ -1,19 +1,33 @@
 from rest_framework import serializers
 from .models import Producto, Orden, DetalleOrden
 
+
+
 class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
         fields = ['id','nombre','precio','stock']
 
-class OrdenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Orden
-        fields = ['id','fecha_hora']
 
 class DetalleOrdenSerializer(serializers.ModelSerializer):
-    orden = OrdenSerializer()
-    producto = ProductoSerializer()
+    #orden = OrdenSerializer()
+    #producto = ProductoSerializer()
     class Meta:
         model = DetalleOrden
-        fields = ['orden','cantidad','producto']
+        fields = '__all__'
+
+class OrdenSerializer(serializers.ModelSerializer):
+    
+    detalles = DetalleOrdenSerializer(many=True, read_only=True)
+    total = serializers.SerializerMethodField()
+    total_usd = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Orden
+        fields = ['id','fecha_hora','detalles','total','total_usd']
+    
+    def get_total(self, orden):
+        return orden.get_total()
+    
+    def get_total_usd(self, orden):
+        return orden.get_total_usd()

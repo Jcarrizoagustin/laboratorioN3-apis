fc9985acf52925a868c8457c23465aa475c16201
jcarrizoagustin <jcarrizoagustin@gmail.com>
2024-05-25 19:15:31 -0300

    Se soluciona error al obtener el valor del dolar en USD

--- core/services.py
+++ core/services.py
@@ -1,4 +1,5 @@
 import requests
+from decimal import Decimal
 
 DOLAR_API_BLUE_URL = 'https://dolarapi.com/v1/dolares/blue'
 
@@ -6,6 +7,6 @@ def get_usd_from_api():
     try:
         response = requests.get(DOLAR_API_BLUE_URL)
         datos = response.json()
-        return datos['venta']
+        return Decimal(datos['venta'])
     except Exception as err:
         print(err)

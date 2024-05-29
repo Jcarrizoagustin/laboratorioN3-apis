553e36380915595c22ef1ca66ab6a7872222687c
jcarrizoagustin <jcarrizoagustin@gmail.com>
2024-05-29 20:59:14 -0300

    Se cambia la ubicacion de la variable DOLAR_BLUE_API_URL

--- core/services.py
+++ core/services.py
@@ -1,11 +1,12 @@
 import requests
 from decimal import Decimal
+from django.conf import settings
 
-DOLAR_API_BLUE_URL = 'https://dolarapi.com/v1/dolares/blue'
+API_URL = settings.DOLAR_BLUE_API_URL
 
 def get_usd_from_api():
     try:
-        response = requests.get(DOLAR_API_BLUE_URL)
+        response = requests.get(API_URL)
         datos = response.json()
         return Decimal(datos['venta'])
     except Exception as err:
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -132,3 +132,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DOLAR_BLUE_API_URL = 'https://dolarapi.com/v1/dolares/blue'

2aa1e367250a149fdb7ad895b6e05880cd5167cd
Daniel_juarez97 <juarezz.daniel97@gmail.com>
2024-06-02 17:45:53 -0300

    sin cambios

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 from .models import Producto, Orden, DetalleOrden
 
+# Register your models here.
+admin.site.register(Producto)
+admin.site.register(Orden)
+admin.site.register(DetalleOrden)
